6a0e45fd69492e15c67bccf43cde1914c810e094
Henno Täht <henno.taht@gmail.com>
2022-01-05 10:13:41 +0200

    Adding a new game functionality

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 8080
 
 const games = [
-    "Witcher 3",
-    "Cyberpunk 2077",
-    "Minecraft",
-    "Counter-Strike: Global Offensive",
-    "Roblox",
-    "Grand Theft Auto V",
-    "Valorant",
-    "Forza Horizon 5",
+    {name: "Witcher 3", price: 29.99},
+    {name: "Cyberpunk 2077", price: 59.99},
+    {name: "Minecraft", price: 26.99},
+    {name: "Counter-Strike: Global Offensive", price: 0},
+    {name: "Roblox", price: 0},
+    {name: "Grand Theft Auto V", price: 29.99},
+    {name: "Valorant", price: 0},
+    {name: "Forza Horizon 5", price: 59.99},
 ]
+
+app.use(express.json())
 
 app.get('/games', (req, res) => {
     res.send(games)
@@ -18,6 +21,24 @@ app.get('/games', (req, res) => {
 
 app.get('/games/:id', (req, res) => {
     res.send(games[req.params.id])
+})
+
+app.post('/games', (req, res) => {
+
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({
+            error: 'One or all params are missing'
+        })
+    }
+
+    games.push({
+        price: req.body.price,
+        name: req.body.name
+    })
+
+    res.status(201).location('localhost:8080/games/' + (games.length - 1)).send(
+        games[games.length - 1]
+    )
 })
 
 app.listen(port, () => {
