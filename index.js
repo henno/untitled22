0b29a97ddcf0b68c433232f15cca1c6d71a27174
Henno Täht <henno.taht@gmail.com>
2022-01-05 09:26:26 +0200

    Add /games

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('express')()
 const port = 8080
 
+app.get('/games', (req, res) => {
+    res.send(["Witcher 3", "Cyberpunk 2077"])
+})
+
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })
